547a710a129e58249ef4e175e21ec4a657f6e6dd
Maciej Stanik <M.Stanik@kainos.com>
2015-07-26 10:28:00 +0200

    Zooming

--- MapApp WatchKit Extension/InterfaceController.swift
+++ MapApp WatchKit Extension/InterfaceController.swift
@@ -13,32 +13,40 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    let location = CLLocationCoordinate2D(latitude: 54.443196, longitude: 18.560582)
+    
     @IBOutlet weak var map: WKInterfaceMap!
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        // Configure interface objects here.
-        
-        let location = CLLocationCoordinate2D(latitude: 54.26, longitude: 18.33)
-        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         var region = MKCoordinateRegion(center: location, span: span)
-        
         map.setRegion(region)
         
         map.addAnnotation(location, withPinColor: WKInterfaceMapPinColor.Red)
-        
     }
-
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-
+    
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    @IBAction func zoomSliderValueChanged(value: Float) {
+        
+        let degrees:CLLocationDegrees = CLLocationDegrees(10 - value) / 10
+        
+        let span = MKCoordinateSpanMake(degrees, degrees)
+        let region = MKCoordinateRegionMake(location, span)
+        
+        map.setRegion(region)
+    }
+    
+    
 }
+
